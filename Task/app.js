c374fa2e36ae8c8485e7e7c285451fc2a402ed3f
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-06-25 13:33:00 -0700

    task has been completed 80%

--- Task/app.js
+++ Task/app.js
@@ -1,24 +1,59 @@
+// data   of Robot
+class Robot{
+    roboWork=["Security  robo","Cleaning  Robo","CookRobo"]
+ getprofile(){
+    throw new Error ("cannot  use this  method   without  override!") 
+ }
+
+ Work(){
+    let random=Math.random() * this.roboWork.length;
+    let index=Math.floor(random)
+return    this.roboWork[index]
+ }
+
+}
 
 
 //   using Encapsolation 
-class IdModel{
+class data extends Robot{
     #models = ["XR-22", "Clean-MAX", "Defender-X", "Vision-9", "FlyBot-1"];
     #i=0
     method(){
         
-        console.log( this.#models[this.#i++]);
+       return  this.#models[this.#i++];
     }
     //to  return  the models
-     get(){
-        return this.method()
-     }
+    
+    getprofile(){
+       let card=document.querySelector(".card-title");
+     return  card.innerHTML= `  Robot :  ${this.method()} <br>Work     : ${this.Work()} `
+
+    }
+    get(){
+        return console.log(this.method())
+    }
 
  }
 
- let x=new IdModel();
- x.get();
- x.get();
- x.get();
+ let Mod=new data();
+ Mod.getprofile();
+ Mod.getprofile();
+
+ let i=0
+
+//   using  genrater function  to show the data of robot and his owner number vise 
+async function* AIPower(){
+let Res=await fetch("https://jsonplaceholder.typicode.com/users");
+let data=await Res.json();
+
+// let card=document.querySelector(".card-title");
+ yield  data[i++].name
+
+
+}
+
+
+
 
 
 
